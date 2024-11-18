ab73ec655bbfd9cc10a4d5ea25384501600ed38b
hyang <yanghuaxuan@gmail.com>
2024-11-18 14:32:05 -0800

    explode

--- gtkwave.rb
+++ gtkwave.rb
@@ -3,16 +3,15 @@ class Gtkwave < Formula
   homepage "https://gtkwave.sourceforge.net"
   license "GPL-2.0-or-later"
   head "https://github.com/gtkwave/gtkwave.git", branch: "master"
-  revision 3
 
+  depends_on "gtk4"
   depends_on "desktop-file-utils" => :build # for update-desktop-database
   depends_on "shared-mime-info" => :build
   depends_on "gobject-introspection" => :build
-  depends_on "gtk-mac-integration" => :build
+   depends_on "gtk-mac-integration" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
-  depends_on "gtk+3"
 
   def install
     ENV["DESTDIR"] = "/"
